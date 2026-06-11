daad64b75ea3d685c31345d6ad5c010e8a2a43e5
Matheus Matos <matheus.matos@sptech.school>
2026-06-11 15:22:14 -0300

    fix: create_file usa printf para interpretar \\n corretamente

--- shared/utils.sh
+++ shared/utils.sh
@@ -4,27 +4,23 @@
 
 SPTECH_HOME="/home/sptech"
 
-# Imprime seção
 section() {
   echo ""
   echo "--- $1 ---"
 }
 
-# Remove e recria diretório (reset limpo)
 reset_dir() {
   local dir="$1"
   rm -rf "$dir"
   mkdir -p "$dir"
 }
 
-# Cria arquivo com conteúdo opcional
 create_file() {
   local path="$1"
   local content="${2:-}"
-  echo "$content" > "$path"
+  printf "%b\n" "$content" > "$path"
 }
 
-# Define dono e permissão
 set_owner() {
   local path="$1"
   local owner="${2:-sptech:sptech}"
